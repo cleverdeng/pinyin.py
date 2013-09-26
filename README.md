99150ab83ad4d17249535b517cd0774c42a4b1df
wklken <ken@Luna-mac.local>
2013-09-26 11:27:48 +0800

    1.增加处理英文数字标点的方法2.增加返回首字母字符串方法3.修复传空格字符串异常

--- README.md
+++ README.md
@@ -3,20 +3,50 @@ pinyin.py
 
 汉字转拼音,With Python
 
-
 Example:
 
     from pinyin import PinYin
-    
+
     test = PinYin()
     test.load_word()
-    test.hanzi2pinyin(string='钓鱼岛是中国的')
+
+    def run_test(string):
+        print
+        print "in: %s" % string
+        print 'convert just Hanzi:'
+        print "out: %s" % ''.join(test.hanzi2pinyin(string=string))
+        print "out: %s" % ''.join(test.hanzi2pinyin(string=string, split="-"))
+        print "out: %s" % test.hanzi2pyacronym(string=string)
+
+        print 'convert all:'
+        print "out: %s" % ''.join(test.all2pinyin(string=string))
+        print "out: %s" % ''.join(test.all2pinyin(string=string, split="-"))
+        print "out: %s" % test.all2pyacronym(string=string)
+        print '-' * 20
+
+
+    #简单字符串
+    string = u'Chrome浏览器版本29'
+    run_test(string)
+
+    #补充例子，复杂字符串
+    string = u'植物大战僵尸2：奇妙时空之旅（Plants vs. Zombies 2: Its About Time）'
+    run_test(string)
 
 
 Out:
 
     test.hanzi2pinyin(string='钓鱼岛是中国的')
-    ['diao', 'yu', 'dao', 'shi', 'zhong', 'guo', 'de']    
+    ['diao', 'yu', 'dao', 'shi', 'zhong', 'guo', 'de']
     test.hanzi2pinyin_split(string='钓鱼岛是中国的', split="-")
     diao-yu-dao-shi-zhong-guo-de
 
+-------
+
+Update:
+
+    2013-09-26  wklken
+                1.[del] split合并到同一个方法中，作为split参数判断处理
+                2.[add] all2pinyin方法，使得模块可以处理返回含有英文，数字，标点符号的字符串
+                3.[add] acronym方法，返回拼音首字母字符串
+                4.[fix] 修复原有方法传入含空格字符串异常的问题
--- pinyin.py
+++ pinyin.py
@@ -6,7 +6,7 @@
     E-mail:clverdeng@gmail.com
 """
 
-__version__ = '0.9'
+__version__ = '1.0'
 __all__ = ["PinYin"]
 
 import os.path
@@ -32,30 +32,89 @@ class PinYin(object):
                     self.word_dict[line[0]] = line[1]
 
 
-    def hanzi2pinyin(self, string=""):
+    def hanzi2pinyin(self, string="", split=None):
+        """
+        只处理中文字符，数字，标点等返回空
+        """
         result = []
         if not isinstance(string, unicode):
             string = string.decode("utf-8")
-        
+
         for char in string:
             key = '%X' % ord(char)
-            result.append(self.word_dict.get(key, char).split()[0][:-1].lower())
 
+            pychar = self.word_dict.get(key, char)
+            if pychar != char:
+                pychar = pychar.split()[0][:-1].lower()
+
+            result.append(pychar)
+
+        if split:
+            return split.join(result)
         return result
 
 
-    def hanzi2pinyin_split(self, string="", split=""):
+    def hanzi2pyacronym(self, string):
+        """
+        拼音首字母缩写
+        """
         result = self.hanzi2pinyin(string=string)
-        if split == "":
-            return result
-        else:
-            return split.join(result)
+        return ''.join([c[0] for c in result if c])
 
+
+    def all2pinyin(self, string="", split=None):
+        """
+        中文字符返回拼音，数字，标点等返回原始字符
+        """
+        result = []
+        if not isinstance(string, unicode):
+            string = string.decode("utf-8")
+
+        for char in string:
+            key = '%X' % ord(char)
+
+            pychar = self.word_dict.get(key, char)
+            if pychar != char:
+                pychar = pychar.split()[0][:-1].lower()
+                pychar = pychar if pychar else char
+
+            result.append(pychar)
+
+        if split:
+            return split.join(result)
+        return result
+
+
+    def all2pyacronym(self, string):
+        """
+        拼音首字母缩写,包含原有字符串英文，标点和数字
+        """
+        result = self.all2pinyin(string=string)
+        return ''.join([c[0] for c in result if c])
 
 if __name__ == "__main__":
     test = PinYin()
     test.load_word()
-    string = "钓鱼岛是中国的"
-    print "in: %s" % string
-    print "out: %s" % str(test.hanzi2pinyin(string=string))
-    print "out: %s" % test.hanzi2pinyin_split(string=string, split="-")
+
+    def run_test(string):
+        print
+        print "in: %s" % string
+        print 'convert just Hanzi:'
+        print "out: %s" % ''.join(test.hanzi2pinyin(string=string))
+        print "out: %s" % ''.join(test.hanzi2pinyin(string=string, split="-"))
+        print "out: %s" % test.hanzi2pyacronym(string=string)
+
+        print 'convert all:'
+        print "out: %s" % ''.join(test.all2pinyin(string=string))
+        print "out: %s" % ''.join(test.all2pinyin(string=string, split="-"))
+        print "out: %s" % test.all2pyacronym(string=string)
+        print '-' * 20
+
+
+    #简单字符串
+    string = u'Chrome浏览器版本29'
+    run_test(string)
+
+    #补充例子，复杂字符串
+    string = u'植物大战僵尸2：奇妙时空之旅（Plants vs. Zombies 2: Its About Time）'
+    run_test(string)
