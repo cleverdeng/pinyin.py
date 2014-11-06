7aa3c83ec2508bec05b5c43bc7883e8b4543bfa8
bottleimp <anakin.yan@gmail.com>
2014-11-06 11:47:29 +0800

    avoid cp word.data to main project dir
    1. no more need to copy the word.data file to current work dir.
    2. some minor changes in setup.py

--- pinyin/pinyin.py
+++ pinyin/pinyin.py
@@ -4,45 +4,34 @@
 """
     Author:cleverdeng
     E-mail:clverdeng@gmail.com
+    Modified by anakin.yan@gmail.com
 """
 
-__version__ = '0.9'
 __all__ = ["PinYin"]
 
-import os.path
+import pkgutil
 
 
 class PinYin(object):
-    def __init__(self, dict_file='word.data'):
+    def __init__(self):
+        __package__ = 'pinyin'
         self.word_dict = {}
-        self.dict_file = dict_file
-
+        self.data = pkgutil.get_data(__package__, 'data/word.data')
 
     def load_word(self):
-        if not os.path.exists(self.dict_file):
-            raise IOError("NotFoundFile")
-
-        with file(self.dict_file) as f_obj:
-            for f_line in f_obj.readlines():
-                try:
-                    line = f_line.split('    ')
-                    self.word_dict[line[0]] = line[1]
-                except:
-                    line = f_line.split('   ')
-                    self.word_dict[line[0]] = line[1]
-
+        for line in self.data.split('\n'):
+            self.word_dict[line[:5].strip()] = line[8:]
 
     def hanzi2pinyin(self, string=""):
         result = []
         if not isinstance(string, unicode):
             string = string.decode("utf-8")
-        
+
         for char in string:
             key = '%X' % ord(char)
             result.append(self.word_dict.get(key, char).split()[0][:-1].lower())
 
         return result
-
 
     def hanzi2pinyin_split(self, string="", split=""):
         result = self.hanzi2pinyin(string=string)
